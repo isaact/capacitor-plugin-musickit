2a5f95f84268b01a7e43dfe612b60a18f44d9d09
Isaac Tewolde <isaac@tewolde.co>
2025-08-19 12:20:07 +0200

    added types

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,25 @@ const CapacitorMusicKit = registerPlugin<CapacitorMusicKitPlugin>(
 );
 
 export * from "./definitions";
+
+// Export MusicKit types for direct import
+export type Playlists = MusicKit.Playlists;
+export type LibraryPlaylists = MusicKit.LibraryPlaylists;
+export type Albums = MusicKit.Albums;
+export type Songs = MusicKit.Songs;
+export type Artists = MusicKit.Artists;
+export type MusicVideos = MusicKit.MusicVideos;
+export type Curators = MusicKit.Curators;
+export type AppleCurators = MusicKit.AppleCurators;
+export type Stations = MusicKit.Stations;
+export type Genres = MusicKit.Genres;
+export type Storefronts = MusicKit.Storefronts;
+export type LibraryAlbums = MusicKit.LibraryAlbums;
+export type LibrarySongs = MusicKit.LibrarySongs;
+export type LibraryArtists = MusicKit.LibraryArtists;
+export type Activities = MusicKit.Activities;
+export type PersonalRecommendation = MusicKit.PersonalRecommendation;
+export type RecordLabels = MusicKit.RecordLabels;
+export type Ratings = MusicKit.Ratings;
+
 export { CapacitorMusicKit };
